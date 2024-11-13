283279a54dc41a9881eb7a46daeda12f6ff4b603
Nathalia Avila <nathyavila05@gmail.com>
2024-11-13 13:13:53 -0500

    Crear página de Post Product, previsualización de imagen y selección de categoría.

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -8,13 +8,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DemoAngularMaterialModule } from 'src/app/DemoAngularMaterialModule';
 import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { PostProductComponent } from './components/post-product/post-product.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     DashboardComponent,
-    PostCategoryComponent
+    PostCategoryComponent,
+    PostProductComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/admin/service/admin.service.ts
+++ src/app/modules/admin/service/admin.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { UserStorageService } from 'src/app/auth/auth-services/storage-service/user-storage.service';
 
 const BASIC_URL = 'http://localhost:8080/';
@@ -14,15 +14,63 @@ export class AdminService {
 
   addCategory(categoryDto: any): Observable<any> {
     console.log(categoryDto);
-    return this.http.post(BASIC_URL + 'api/admin/category', categoryDto, {
+    return this.http.post(`${BASIC_URL}api/admin/category`, categoryDto, {
       headers: this.createAuthorizationHeader(),
     })
   }
 
+  getAllCategories(): Observable<any> {
+    return this.http.get(`${BASIC_URL}api/admin/categories`, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  addProduct(productDto: any): Observable<any> {
+    return this.http.post<[]>(`${BASIC_URL}api/admin/product`, productDto, {
+      headers: this.createAuthorizationHeader(),
+    }).pipe(
+      tap((_) => this.log('Product posted successfully')),
+      catchError(this.handleError<[]>('Error posting Product', []))
+    );
+  }
+
+  getAllProducts(): Observable<any> {
+    return this.http.get<[]>(`${BASIC_URL}api/admin/products`, {
+      headers: this.createAuthorizationHeader(),
+    }).pipe(
+      tap((_) => this.log('Products fetched successfully')),
+      catchError(this.handleError<[]>('Error getting Products', []))
+    );
+  }
+
+  getProductById(productId: any): Observable<any> {
+    return this.http.get<[]>(`${BASIC_URL}api/admin/product/${productId}`, {
+      headers: this.createAuthorizationHeader(),
+    }).pipe(
+      tap((_) => this.log('Product Fetched successfully')),
+      catchError(this.handleError<[]>('Error Getting Product', []))
+    );
+  }
+
+  // Other methods
+
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
-      'Authorization', 'Bearer ' + UserStorageService.getToken()
+      'Authorization',
+      'Bearer ' + UserStorageService.getToken()
     );
+  }
+
+  private log(message: string): void {
+    console.log(`Admin Service: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T): any {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 
 }
